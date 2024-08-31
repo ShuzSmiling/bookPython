926ad75196f252dbc892910c1b71deb37474f146
Alex ch <shuzsmiling@gmail.com>
2024-08-31 13:45:14 +0300

    2.0-2.12

---
+++ oneBook/dollar_display.py
@@ -0,0 +1,3 @@
+mounthly_pay = 5000.0
+annual_pay = mounthly_pay * 12
+print(f'Ваша годовая зарпалата составляет ${annual_pay:,.2f}')
---
+++ oneBook/f_string_no_formatting.py
@@ -0,0 +1,4 @@
+amount_due = 5000.0
+monthly_payment = amount_due / 12.0
+
+print(f'Ежемесячный платеж составляет {monthly_payment}')
